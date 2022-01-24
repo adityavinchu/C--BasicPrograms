250e0ad344e49f917f59b6ed999da776c4c1faa4
adityavinchu <88549097+adityavinchu@users.noreply.github.com>
2022-01-24 12:32:20 +0530

    Add files via upload

---
+++ Area/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicCoreProg
+{ 
+	public class Circle
+	{
+		float radius;
+		public areaOfCircle()
+		{
+
+			Console.WriteLine("Enter Radius: ");
+			radius = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Area of circle: {0}", 3.14 * radius * radius);
+		}
+	}
+}
